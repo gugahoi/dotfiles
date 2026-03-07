536ce2b9ace3ab76ad2d7596aa2495c9d4d650bf
Gustavo Hoirisch <guga@MacBookAir.localdomain>
2026-03-07 22:41:34 +1100

    feat(snacks): add more keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,4 +1,4 @@
-vim.pack.add({"https://github.com/folke/snacks.nvim"})
+vim.pack.add({ "https://github.com/folke/snacks.nvim" })
 local Snacks = require("snacks")
 
 Snacks.setup({
@@ -6,27 +6,32 @@ Snacks.setup({
 	notifier = { enabled = true },
 	picker = {
 		sources = {
-			explorer = {}, 
+			explorer = {},
 			files = { hidden = true },
 			grep = {},
 		},
-	}
+	},
 })
 
 local keymaps = {
+    -- stylua: ignore start
     -- Top Pickers & Explorer
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+
     { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
     { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
     { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
-    { "<leader><leader>", function() Snacks.picker.buffers() end, desc = "Open Buffers" },
-    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
     { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
     { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Visual selection or word", mode = { "n", "x" } },
     { "<leader>sm", function() Snacks.picker.man() end, desc = "Man Pages" },
+    { "<leader>s\"", function() Snacks.picker.registers() end, desc = "Registers" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
     { "<leader>sh", function() Snacks.picker.help() end, desc = "Neovim Help" },
+    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "Document Symbols" },
+	-- stylua: ignore end
 }
 
 for _, map in ipairs(keymaps) do
