4e87f28eeb94557ce2f4e6d67029781c0e70053c
Gustavo Hoirisch <guga@MacBookAir.localdomain>
2026-03-07 22:42:11 +1100

    feat(comments): add todo-comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,13 @@
-require("plugins.theme")
+vim.pack.add({
+	"https://github.com/folke/todo-comments.nvim",
+})
+require("todo-comments").setup()
+
 require("plugins.amp")
-require("plugins.snacks")
-require("plugins.treesitter")
-require("plugins.lsp")
 require("plugins.format")
 require("plugins.git")
+require("plugins.lsp")
+require("plugins.snacks")
+require("plugins.theme")
+require("plugins.tmux")
+require("plugins.treesitter")
