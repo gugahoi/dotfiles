27d7f0d49770a50ed3303aaf0f2e424922c4910e
Gustavo Hoirisch <355877+gugahoi@users.noreply.github.com>
2026-03-21 14:28:26 +1100

    chore(nvim): delete commented out code

--- .config/nvim/lua/plugins/picker.lua
+++ .config/nvim/lua/plugins/picker.lua
@@ -87,16 +87,3 @@ for _, map in ipairs(keymaps) do
     local mode = map.mode or "n"
     vim.keymap.set(mode, map[1], map[2], opts)
 end
-
--- Grapple options
--- opts = {
---     scope = "git", -- also try out "git_branch"
--- },
--- event = { "BufReadPost", "BufNewFile" },
--- cmd = "Grapple",
--- keys = {
---     { "<leader>m", "<cmd>Grapple toggle<cr>", desc = "Grapple toggle tag" },
---     { "<leader>M", "<cmd>Grapple toggle_tags<cr>", desc = "Grapple open tags window" },
---     { "<leader>n", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
---     { "<leader>p", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
--- }
