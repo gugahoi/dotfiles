63179e35c355f7d0195f3a69e24cb61cd1320d66
Gustavo Hoirisch <355877+gugahoi@users.noreply.github.com>
2026-03-27 22:01:18 +1100

    feat(nvim): add system clipboard keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,10 @@ map("n", "<Esc>", ":nohlsearch<cr>", { silent = true })
 
 -- Exit terminal mode
 map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- stylua: ignore start
+vim.keymap.set({ "n", "v" }, "gy", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set( { "n", "v" }, "gY", '"+Y', { desc = "Yank line to system clipboard" })
+vim.keymap.set( "n", "gp", '"+p', { desc = "Paste from system clipboard after cursor" })
+vim.keymap.set( "n", "gP", '"+P', { desc = "Paste from system clipboard before cursor" })
+-- stylua: ignore end
