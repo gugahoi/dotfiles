3e4f7ec306cc35312d333e9f85e7dca48ff6f490
Gustavo Hoirisch <355877+gugahoi@users.noreply.github.com>
2026-05-11 13:01:25 +1000

    feat(nvim): enable auto completion on lsp
    For lsps that support autocompletion, we should see them suggested as we
    type.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -94,6 +94,22 @@ local function setup_keymaps(bufnr)
     end, opts)
 end
 
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+        if client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(
+                true,
+                client.id,
+                ev.buf,
+                { autotrigger = true }
+            )
+        end
+    end,
+})
+vim.opt.complete:append("o")
+vim.opt.completeopt = { "menuone", "noselect" }
+
 -- Attach keymaps on LSP attach
 ---@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
