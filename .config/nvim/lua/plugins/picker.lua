e7bf8656d35235fea443c032d5a9abca1d50f5b0
Gustavo Hoirisch <355877+gugahoi@users.noreply.github.com>
2026-05-25 15:15:08 +1000

    feat(nvim): add recent files picker keymap

--- .config/nvim/lua/plugins/picker.lua
+++ .config/nvim/lua/plugins/picker.lua
@@ -49,6 +49,7 @@ local keymaps = {
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
     { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
     { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files (Snacks)" },
+    { "<leader>fh", function() Snacks.picker.recent() end, desc = "Find Recent Files (Snacks)" },
     { "<leader>fn", function() Snacks.picker.notifications() end, desc = "Notifications" },
     { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>fe", function() Snacks.explorer() end, desc = "File Explorer" },
